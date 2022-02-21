db1112fb1de7f10ee7bd75e41eb8c2088ce78a28
kontrast7 <maks.mashko@gmail.com>
2022-02-21 11:56:58 +0300

    add gh-pages

--- src/Components/Header/header.js
+++ src/Components/Header/header.js
@@ -1,13 +1,9 @@
 import React from "react";
 import logo from "../../Pic/Header/Group_6.png";
 import "./header.css";
-
-
 import Home from "../Home/index";
-import Item from "../Item1/index";
 
-
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { HashRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 class Header extends React.Component {
   showSettings(event) {
@@ -41,60 +37,17 @@ class Header extends React.Component {
               <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                 <Link to="/">
                   <li className="nav-item">
-                    {/*<span className="nav-link">*/}
-                    {/*  home*/}
-                    {/*</span>*/}
+                    <span className="nav-link">
+                      home
+                    </span>
                   </li>
                 </Link>
-                {/*<Link to="/item1">*/}
-                {/*  <li className="nav-item">*/}
-                {/*    <span className="nav-link">*/}
-                {/*      demos*/}
-                {/*    </span>*/}
-                {/*  </li>*/}
-                {/*</Link>*/}
-                {/*<li className="nav-item">*/}
-                {/*  <span className="nav-link">*/}
-                {/*    features*/}
-                {/*  </span>*/}
-                {/*</li>*/}
-
-                {/*<li className="nav-item">*/}
-                {/*  <span className="nav-link">*/}
-                {/*    shortcodes*/}
-                {/*  </span>*/}
-                {/*</li>*/}
-
-                {/*<li className="nav-item">*/}
-                {/*  <span className="nav-link">*/}
-                {/*    shop*/}
-                {/*  </span>*/}
-                {/*</li>*/}
-
-                {/*<li className="nav-item">*/}
-                {/*  <span className="nav-link">*/}
-                {/*    blog*/}
-                {/*  </span>*/}
-                {/*</li>*/}
-
-                {/*<li className="nav-item">*/}
-                {/*  <span className="nav-link">*/}
-                {/*    portfolio*/}
-                {/*  </span>*/}
-                {/*</li>*/}
-
-                {/*<li className="nav-item">*/}
-                {/*  <span className="nav-link header-search">*/}
-                {/*    <img src={search} className="header-search-img" alt="search"></img>*/}
-                {/*  </span>*/}
-                {/*</li>*/}
               </ul>
             </div>
           </div>
         </nav>
         <Switch>
           <Route path="/" exact component={Home} />
-          <Route path="/item1" component={Item} />
         </Switch>
       </Router>
     );
